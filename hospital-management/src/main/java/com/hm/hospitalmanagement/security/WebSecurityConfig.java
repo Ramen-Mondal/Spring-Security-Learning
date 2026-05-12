3d8688497d4f20ed3bfc8dd91c5275f716e29ea2
Ramen Mondal <rmofficial01x@gmail.com>
2026-05-12 15:28:30 +0530

    JwtAuthFilter making which is a our server's own filter

--- hospital-management/src/main/java/com/hm/hospitalmanagement/security/WebSecurityConfig.java
+++ hospital-management/src/main/java/com/hm/hospitalmanagement/security/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,11 +13,13 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
+    private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception{
@@ -29,10 +32,12 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/public/**","/auth/login","/auth/signup").permitAll()
 //                        .requestMatchers("/admin/**").authenticated()
-                        .requestMatchers("/admin/**").hasRole("ADMIN")
-                                .requestMatchers("/doctor/**").hasAnyRole("DOCTOR","ADMIN")
-//                                .anyRequest().authenticated()
-                );
+//                        .requestMatchers("/admin/**").hasRole("ADMIN")
+//                                .requestMatchers("/doctor/**").hasAnyRole("DOCTOR","ADMIN")
+                                .anyRequest().authenticated()
+                )
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+        ;
 //                .formLogin(Customizer.withDefaults());
 
 
