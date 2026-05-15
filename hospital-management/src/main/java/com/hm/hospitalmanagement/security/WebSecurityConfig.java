9beeefc9d2d91eba857442d33541a9a6c8dbaa73
Ramen Mondal <rmofficial01x@gmail.com>
2026-05-15 10:55:37 +0530

    Login with oAuth2 Client and also setup google oAuth2 client and save it in invairoment variable also ignore code in gitignore file and add a dependency for spring auto detect env

--- hospital-management/src/main/java/com/hm/hospitalmanagement/security/WebSecurityConfig.java
+++ hospital-management/src/main/java/com/hm/hospitalmanagement/security/WebSecurityConfig.java
@@ -1,6 +1,10 @@
 package com.hm.hospitalmanagement.security;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -11,13 +15,18 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@Slf4j
 public class WebSecurityConfig {
     private final JwtAuthFilter jwtAuthFilter;
 
@@ -37,6 +46,12 @@ public class WebSecurityConfig {
                                 .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .oauth2Login(oAuth2 ->oAuth2.failureHandler(
+                        (request, response, exception) -> {
+                            log.error("OAuth2 error: {}", exception.getMessage());
+
+                        }
+                ))
         ;
 //                .formLogin(Customizer.withDefaults());
 
